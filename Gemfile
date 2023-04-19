e9790cccbc9c7f017d83466a79871b2a2dcf75d4
briigeta <bridgetdkabouet@gmail.com>
2023-04-19 21:06:42 +0100

    Changes to be committed: 	modified:   Gemfile 	modified:   Gemfile.lock 	modified:   config/environments/development.rb 	new file:   config/initializers/devise.rb 	new file:   config/locales/devise.en.yml 	modified:   config/routes.rb

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby "3.2.2"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.3"
 
+gem 'devise', '~> 4.9', '>= 4.9.2'
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
@@ -37,7 +38,7 @@ gem "jbuilder"
 # gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
+gem "tzinfo-data"
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'static_page/landing'
-  get 'static_page/home'
-  get 'static_page/writing'
-  get 'static_page/profile'
+  #get 'static_page/landing'
+  #get 'static_page/home'
+  #get 'static_page/writing'
+  #get 'static_page/profile'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root to:"static_page#landing"
+   get '/home', to: 'static_page#home'
+   get '/writing', to: 'static_page#writing'
+   get '/profile', to: 'static_page#profile'
+
+
 end
