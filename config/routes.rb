0e0341148e473467e203691fab9501f35853c324
briigeta <bridgetdkabouet@gmail.com>
2023-04-20 00:58:33 +0100

    # Changes to be committed:     #	new file:   .sfdx/tools/apex.db     #	modified:   Gemfile     #	modified:   Gemfile.lock     #	modified:   app/javascript/application.js     #	modified:   app/views/layouts/application.html.erb     #	modified:   config/routes.rb     #	new file:   db/schema.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  #get 'static_page/landing'
-  #get 'static_page/home'
-  #get 'static_page/writing'
-  #get 'static_page/profile'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  devise_scope :user do
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
    root to:"static_page#landing"
    get '/home', to: 'static_page#home'
    get '/writing', to: 'static_page#writing'
    get '/profile', to: 'static_page#profile'
-
 
 end
