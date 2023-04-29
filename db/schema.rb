b924e43b992f5746037044c9faaa545a2452fdd5
briigeta <bridgetdkabouet@gmail.com>
2023-04-29 20:52:33 +0200

    # Changes to be committed:     #	deleted:    app/assets/stylesheets/application.css.scss     #	new file:   app/models/poem.rb     #	modified:   app/views/layouts/application.html.erb     #	deleted:    app/views/shared/_navbar.html.erb     #	modified:   app/views/static_page/home.html.erb     #	new file:   db/migrate/20230429004343_create_poems.rb     #	modified:   db/schema.rb     #	new file:   test/fixtures/poems.yml     #	new file:   test/models/poem_test.rb     #

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_19_201913) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_29_004343) do
+  create_table "poems", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
