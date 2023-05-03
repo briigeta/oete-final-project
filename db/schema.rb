38617d9d3e356d32c05ff8e66c650721f6928ad9
briigeta <bridgetdkabouet@gmail.com>
2023-05-03 07:17:39 +0200

    yayay  Changes to be committed: 	modified:   app/assets/stylesheets/application.scss  new file:   app/assets/stylesheets/devise.scss 	new file:   app/assets/stylesheets/forms.scss 	modified:   app/helpers/application_helper.rb 	new file:   app/views/devise/confirmations/new.html.erb 	new file:   app/views/devise/mailer/confirmation_instructions.html.erb 	new file:   app/views/devise/mailer/email_changed.html.erb 	new file:   app/views/devise/mailer/password_change.html.erb 	new file:   app/views/devise/mailer/reset_password_instructions.html.erb 	new file:   app/views/devise/mailer/unlock_instructions.html.erb 	new file:   app/views/devise/passwords/edit.html.erb 	new file:   app/views/devise/passwords/new.html.erb 	new file:   app/views/devise/registrations/edit.html.erb 	new file:   app/views/devise/registrations/new.html.erb 	new file:   app/views/devise/sessions/new.html.erb 	new file:   app/views/devise/shared/_error_messages.html.erb     	new file:   app/views/devise/shared/_links.html.erb new file:   app/views/devise/unlocks/new.html.erb 	modified:   app/views/layouts/application.html.erb 	new file:   db/migrate/20230503024654_add_username_to_users.rb 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_29_004343) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_024654) do
   create_table "poems", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -26,8 +26,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_29_004343) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
